bc516fb23737aa6c183a78e3d810f7e9307eefd2
Andrey Klimkov <aaklimkov@gmail.com>
2024-03-19 07:56:45 +0300

    feat: prepare to publish

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Modal from "./components/Modal/Modal";
+import TextField from "./components/TextField/TextField";
+import Select from "./components/Select/Select";
+import Button from "./components/Button/Button";
+import Switch from "./components/Switch/Switch";
+import Checkbox from "./components/Checkbox/Checkbox";
+
+export { Modal, TextField, Select, Button, Switch, Checkbox };
